6b016cfc558a8dfa5472b1819d48285b437927e2
Sudip Adhikari <sudip.adhk1@gmail.com>
2023-12-12 07:43:13 +0545

    Canvas and BGcolor added

--- script.js
+++ script.js
@@ -1,54 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const clipboard = document.getElementById('clipboard');
-    const selection = document.getElementById('matrixselect'); // Corrected ID
-    const tools = document.getElementById('tools');
-    const brush = document.getElementById('brush');
-    const shapes = document.getElementById('shapes');
-    const size = document.getElementById('size');
-
-    let matrixBox = document.getElementById("matrixBox");
-    let matrixClip = document.getElementById("matrixClip");
-    let matrixSelect = document.getElementById("matrixselect");
-    let matrixBrush = document.getElementById("matrixbrush");
-    let matrixShapes = document.getElementById("matrixshapes");
-    let matrixSize = document.getElementById("matrixsize");
-
-    tools.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixBox);
-    });
-    
-
-    clipboard.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixClip);
-    });
-
-    selection.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixSelect);
-    });
-
-    brush.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixBrush);
-    });
-
-    shapes.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixShapes);
-    });
-
-    size.addEventListener("click", function (e) {
-        toggleMatrixContainer(matrixSize);
-    });
-
-    function toggleMatrixContainer(matrixContainer) {
-        matrixContainer.style.display = matrixContainer.style.display === "none" ? "grid" : "none";
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("drawingCanvas");
     const ctx = canvas.getContext("2d");
 
     let isDrawing = false;
-    let currentTool = "pencil";
 
     function startDrawing(e) {
         isDrawing = true;
@@ -57,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function stopDrawing() {
         isDrawing = false;
-       
     }
 
     function draw(e) {
@@ -65,16 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
         ctx.lineWidth = 2;
         ctx.lineCap = "round";
-        ctx.strokeStyle = currentTool === "eraser" ? "#ffffff" : "#000000";
+        ctx.strokeStyle = "#000000";
 
         ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
         ctx.stroke();
-       
+
         ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     }
 
-    function selectTool(tool) {
-        currentTool = tool;
+    function fillWithColor(color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
     canvas.addEventListener("mousedown", startDrawing);
@@ -82,11 +36,12 @@ document.addEventListener("DOMContentLoaded", function () {
     canvas.addEventListener("mouseup", stopDrawing);
     canvas.addEventListener("mouseout", stopDrawing);
 
-    document.getElementById("pencil").addEventListener("click", function () {
-        selectTool("pencil");
-    });
+    const colorPalette = document.querySelectorAll('.color-container p');
 
-    document.getElementById("eraser").addEventListener("click", function () {
-        selectTool("eraser");
+    colorPalette.forEach(color => {
+        color.addEventListener('click', function (e) {
+            const selectedColor = getComputedStyle(e.target).backgroundColor;
+            fillWithColor(selectedColor);
+        });
     });
 });
